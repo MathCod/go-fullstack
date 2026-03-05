a8282250903ab3aad1308364445a056339fdccda
Mathias BERGER <mathias.codage@gmail.com>
2026-03-05 15:00:24 +0100

    Login & SignUp

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,6 +6,6 @@ const userSchema = mongoose.Schema({
   password: { type: String, required: true }
 });
 
-userSchema.plugin(uniqueValidator);
+userSchema.plugin(uniqueValidator.default);
 
 module.exports = mongoose.model('User', userSchema);
